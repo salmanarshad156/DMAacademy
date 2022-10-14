1e8557b4ffa76ee8fabbbc7a1b4da53a214bfeab
salmanarshad156 <85060599+salmanarshad156@users.noreply.github.com>
2022-10-14 17:21:28 +0500

    changes

--- footer.php
+++ footer.php
@@ -1,37 +1,55 @@
 <footer>
-    <section class="Signup">
-        <div class="Signup__text">
-            <h5 class="text-green text-center mb-4" style="font-size: 1.6rem !important;">Join our mailing list</h5>
-        </div>
-        <form class="Signup__form" id="newsletter">
-            <input required id="email" type="email" placeholder="Your e-mail">
-            <button form="newsletter" type="submit" class="btn-default">Subscribe</button>
-        </form>
-    </section>
-    <div class="bg-white py-3 py-lg-5">
+
+    <div class="bg-white py-4">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-lg-6">
-                    <img src="assets/img/logo.png" alt="">
+                <div class="col-lg-3">
+                    <div class="footer-logo">
+                        <img src="assets/img/footer-logo.png" alt="">
+                    </div>
                 </div>
-                <div class="col-lg-6 pt-3 pt-lg-0">
+                <div class="col-lg-6 py-3 py-lg-0">
+                    <section class="Signup">
+                        <div class="Signup__text">
+                            <h5 class="text-green text-center mb-4" style="font-size: 1.8rem !important;">Join our
+                                mailing list</h5>
+                        </div>
+                        <form id="newsletter">
+                            <div class="Signup__form">
+                                <input required id="email" type="email" placeholder="Your e-mail">
+                                <button form="newsletter" type="submit" class="btn-default">Subscribe</button>
+
+                            </div>
+                            <div class="form-check mt-2">
+                                <input class="form-check-input" name="news-letter" type="checkbox" value="news-letter"
+                                       id="news-letter">
+                                <label class="form-check-label text-green" for="news-letter">
+                                    Check here if you accept our terms
+                                    (<a href="#" class="text-green">privacy policy</a>)
+                                </label>
+                            </div>
+                        </form>
+                    </section>
+                </div>
+                <div class="col-lg-3">
+                    <div>
+                        <a href="#" class="text-green">info@dmaacademy.co.uk</a>
+                    </div>
                     <div class="social-links">
                         <a href="#"><i class="fab fa-instagram"></i></a>
                         <a href="#"><i class="fab fa-facebook"></i></a>
-                      <!--  <a href="#"><i class="fab fa-twitter"></i></a> -->
+                        <a href="#"><i class="fa fa-envelope"></i></a>
+                        <!--  <a href="#"><i class="fab fa-twitter"></i></a> -->
                     </div>
-                    <div>
-                        <a href="#">info@dmaacademy.co.uk</a>
-                    </div>
-             <!--       <div>
-                        <a href="#">Leeds, 9 Marsden Avenue, LS11 7NS</a>
-                    </div>  -->
+                    <!--       <div>
+                               <a href="#">Leeds, 9 Marsden Avenue, LS11 7NS</a>
+                           </div>  -->
                 </div>
 
             </div>
         </div>
     </div>
-    <div class="bg-green py-3">
+    <div class="bg-green py-3 footer-bottom">
         <div>
             © 2022 DMA Academy-All Rights Reserved
         </div>
@@ -42,5 +60,5 @@
 </footer>
 
 <div class="floating-btn">
-    <a href="https://google.com" target="_blank" class="btn-default">Book Online</a>
+    <a href="https://google.com" target="_blank" class="btn-default d-inline-block">Book Online</a>
 </div>
